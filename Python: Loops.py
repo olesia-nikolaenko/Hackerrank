daa20e27a7496fe3b05bb0b42a23d10787bdb794
olesia-nikolaenko <evavictorn@gmail.com>
2023-02-24 12:08:25 -0800

    Solved exercise

---
+++ Python: Loops.py
@@ -0,0 +1,8 @@
+"""
+The provided code stub reads and integer, n, from STDIN.
+For all non-negative integers i < n, print i**2.
+"""
+n = 5
+for i in range(n):
+    if i >= 0:
+        print(i ** 2)
